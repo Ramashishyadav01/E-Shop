187900fbfa1a21d09a0adadfa54a07687c94daca
Ramashishyadav01 <ramashishyadav42449@gmail.com>
2026-09-23 23:57:41 +0530

    Updated project

--- ecom-node/src/config/db.js
+++ ecom-node/src/config/db.js
@@ -1,28 +1,8 @@
-// const mongoose = require('mongoose');
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/ecommerce');
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error(`MongoDB connection error: ${error.message}`);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-
-
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error('MONGO_URI is not defined');
-    }
-
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/ecommerce');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
@@ -31,3 +11,23 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     if (!process.env.MONGO_URI) {
+//       throw new Error('MONGO_URI is not defined');
+//     }
+
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error(`MongoDB connection error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
